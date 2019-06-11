b2e5e8d8d1f232cd3d47fbbd1d113a40c2f8625a
Pranav Krishnamurthy <pkrish19@outlook.com>
2019-06-11 15:12:47 -0700

    Having the latest changes made

--- Source/CalculatorChatBot/Bots/CalculatorBot.cs
+++ Source/CalculatorChatBot/Bots/CalculatorBot.cs
@@ -1,18 +1,36 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-//
-// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.3.0
-
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.Bot.Builder;
-using Microsoft.Bot.Schema;
+// <copyright file="CalculatorBot.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
 
 namespace CalculatorChatBot.Bots
 {
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Bot.Builder;
+    using Microsoft.Bot.Schema;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
+
+    /// <summary>
+    /// Initializes the CalculatorBot class.
+    /// </summary>
     public class CalculatorBot : ActivityHandler
     {
+        private readonly IConfiguration configuration;
+        private readonly ILogger<CalculatorBot> logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CalculatorBot"/> class.
+        /// </summary>
+        /// <param name="configuration">The current configuration.</param>
+        /// <param name="logger">Use of the logging mechanisms.</param>
+        public CalculatorBot(IConfiguration configuration, ILogger<CalculatorBot> logger)
+        {
+            this.logger = logger;
+            this.configuration = configuration;
+        }
+
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
             await turnContext.SendActivityAsync(MessageFactory.Text($"Echo: {turnContext.Activity.Text}"), cancellationToken);
