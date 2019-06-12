db5f78000996bedf55b6b0959e1650b13e5d203a
Pranav Krishnamurthy <pkrish19@outlook.com>
2019-06-11 21:59:47 -0700

    Having the necessary things done for the adaptive card - next is to wire everything up

--- Source/CalculatorChatBot/Helpers/AdaptiveCards/WelcomeAdaptiveCard.cs
+++ Source/CalculatorChatBot/Helpers/AdaptiveCards/WelcomeAdaptiveCard.cs
@@ -1,11 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿// <copyright file="WelcomeAdaptiveCard.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
 
 namespace CalculatorChatBot.Helpers.AdaptiveCards
 {
+    using System.Collections.Generic;
+    using System.IO;
+    using CalculatorChatBot.Properties;
+
+    /// <summary>
+    /// This class is responsible for the generation of the welcome adaptive card.
+    /// </summary>
     public class WelcomeAdaptiveCard
     {
+        private static readonly string CardTemplate;
+
+        static WelcomeAdaptiveCard()
+        {
+            var cardJsonFilePath = Path.Combine(".", "Helpers", "AdaptiveCards", "WelcomeAdaptiveCard.json");
+            CardTemplate = File.ReadAllText(cardJsonFilePath);
+        }
+
+        /// <summary>
+        /// Generates the JSON string of the adaptive card.
+        /// </summary>
+        /// <param name="botDisplayName">The display name for the bot.</param>
+        /// <returns>JSON string of the welcome adaptive card.</returns>
+        public static string GetCard(string botDisplayName)
+        {
+            var welcomeCardTitleText = Resources.WelcomeCardTitle;
+            var welcomeCardContentPart1 = string.Format(Resources.WelcomeCardContentPart1, botDisplayName);
+            var welcomeCardContentPart2 = Resources.WelcomeCardContentPart2;
+            var welcomeCardBulletListItem1 = Resources.WelcomeCardBulletListItem1;
+            var welcomeCardBulletListItem2 = Resources.WelcomeCardBulletListItem2;
+            var welcomeCardBulletListItem3 = Resources.WelcomeCardBulletListItem3;
+            var takeATourText = Resources.TakeATourText;
+
+            var variablesToValues = new Dictionary<string, string>()
+            {
+                { "welcomeCardTitleText", welcomeCardTitleText },
+                { "welcomeCardContentPart1", welcomeCardContentPart1 },
+                { "welcomeCardContentPart2", welcomeCardContentPart2 },
+                { "welcomeCardBulletListItem1", welcomeCardBulletListItem1 },
+                { "welcomeCardBulletListItem2", welcomeCardBulletListItem2 },
+                { "welcomeCardBulletListItem3", welcomeCardBulletListItem3 },
+                { "takeATourText", takeATourText },
+            };
+
+            var cardBody = CardTemplate;
+            foreach (var kvp in variablesToValues)
+            {
+                cardBody = cardBody.Replace($"%{kvp.Key}%", kvp.Value);
+            }
+
+            return cardBody;
+        }
     }
 }
