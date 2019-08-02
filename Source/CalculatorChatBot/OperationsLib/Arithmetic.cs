75ecbb677bd6e7615c80ab3c2db94e5f26e52c40
Pranav Krishnamurthy <pkrish19@outlook.com>
2019-08-02 12:13:24 -0700

    Create Arithmetic.cs
    Having the first commit on this branch

---
+++ Source/CalculatorChatBot/OperationsLib/Arithmetic.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="Arithmetic.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace CalculatorChatBot.OperationsLib
+{
+    /// <summary>
+    /// This class represents all of the arithmetic operations that are to be done.
+    /// </summary>
+    public class Arithmetic
+    {
+    }
+}
