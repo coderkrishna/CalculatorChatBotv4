ede2a8c75e4f6a19a67fb34a65b229019421b94d
Pranav Krishnamurthy <v-prkris@microsoft.com>
2020-01-22 13:32:25 -0800

    Having things properly done with the operations

--- Source/CalculatorChatBot/OperationsLib/Geometric.cs
+++ Source/CalculatorChatBot/OperationsLib/Geometric.cs
@@ -4,11 +4,13 @@
 
 namespace CalculatorChatBot.OperationsLib
 {
-    using Microsoft.ApplicationInsights;
-    using Microsoft.Bot.Builder;
     using System;
+    using System.Globalization;
     using System.Threading;
     using System.Threading.Tasks;
+    using CalculatorChatBot.Properties;
+    using Microsoft.ApplicationInsights;
+    using Microsoft.Bot.Builder;
 
     /// <summary>
     /// This class implements the methods that are defined in <see cref="IGeometric"/>.
@@ -26,15 +28,32 @@ namespace CalculatorChatBot.OperationsLib
             this.telemetryClient = telemetryClient;
         }
 
+        /// <summary>
+        /// This method will calculate the discriminant.
+        /// </summary>
+        /// <param name="inputList">The list of integers.</param>
+        /// <param name="turnContext">The current turn/execution flow.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>A unit of execution.</returns>
         public async Task CalculateDiscriminant(
             string inputList,
             ITurnContext turnContext,
             CancellationToken cancellationToken)
         {
+            var provider = CultureInfo.InvariantCulture;
+            this.telemetryClient.TrackTrace($"CalculateDiscriminant start at: {DateTime.Now.ToString("O", provider)}");
+
             if (inputList is null)
             {
                 throw new ArgumentNullException(nameof(inputList));
             }
+
+            if (turnContext is null)
+            {
+                throw new ArgumentNullException(nameof(turnContext));
+            }
+
+            await turnContext.SendActivityAsync(MessageFactory.Text(Resources.CurrentMethodBeingImplementedMessage), cancellationToken);
         }
     }
 }
