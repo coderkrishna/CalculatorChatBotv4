03b3c7dd2473d90b793bc6694f6f1aa8414862fb
Pranav Krishnamurthy (TEAMS) <pkrish19@outlook.com>
2020-06-25 14:14:22 -0700

    Adding the first commit for the necessary changes

---
+++ Source/CalculatorChatBot/Helpers/ResponseCard.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="ResponseCard.cs" company="Tata Consultancy Services Ltd">
+// Copyright (c) Tata Consultancy Services Ltd. All rights reserved.
+// </copyright>
+
+namespace CalculatorChatBot.Helpers
+{
+    using System.Collections.Generic;
+    using Microsoft.Bot.Schema;
+
+    /// <summary>
+    /// This is the class that will return the response cards accordingly.
+    /// </summary>
+    public static class ResponseCard
+    {
+    }
+}
