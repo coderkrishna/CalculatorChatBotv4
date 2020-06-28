6b8fa7a5cec0d5c0bfcfd5bdb963d211dc9b1aa5
Pranav Krishnamurthy <pkrish19@outlook.com>
2020-06-28 15:54:46 -0700

    Rendering the results (#3)
    Having all of the results rendered accordingly, for all the return types

--- Source/CalculatorChatBot/Bots/CalculatorBot.cs
+++ Source/CalculatorChatBot/Bots/CalculatorBot.cs
@@ -8,6 +8,7 @@ namespace CalculatorChatBot.Bots
     using System.Collections.Generic;
     using System.Threading;
     using System.Threading.Tasks;
+    using CalculatorChatBot.Helpers;
     using CalculatorChatBot.OperationsLib;
     using CalculatorChatBot.Properties;
     using Microsoft.ApplicationInsights;
@@ -78,67 +79,86 @@ namespace CalculatorChatBot.Bots
                 var incomingTextArray = turnContext.Activity.Text.Split(' ');
                 var command = incomingTextArray[0];
                 var commandInputList = incomingTextArray[1];
+                Attachment responseAttachment = null;
 
                 switch (command)
                 {
                     case "sum":
                     case "add":
                         var sum = this.arithmetic.CalculateSum(commandInputList, turnContext, cancellationToken);
+                        responseAttachment = ResponseCard.GetCardWithIntResult(sum, command);
                         break;
                     case "difference":
                     case "minus":
                         var diff = this.arithmetic.CalculateDifference(commandInputList, turnContext, cancellationToken);
+                        responseAttachment = ResponseCard.GetCardWithIntResult(diff, command);
                         break;
                     case "multiplication":
                     case "product":
                         var product = this.arithmetic.CalculateProduct(commandInputList, turnContext, cancellationToken);
+                        responseAttachment = ResponseCard.GetCardWithIntResult(product, command);
                         break;
                     case "division":
                     case "quotient":
                         var quotient = this.arithmetic.CalculateQuotient(commandInputList, turnContext, cancellationToken);
+                        responseAttachment = ResponseCard.GetCardWithDecimalResult(quotient, command);
                         break;
                     case "mean":
                     case "average":
                         var mean = this.statistics.CalculateMean(commandInputList, turnContext, cancellationToken);
+                        responseAttachment = ResponseCard.GetCardWithDecimalResult(mean, command);
                         break;
                     case "median":
                     case "middle of the list":
                         var median = this.statistics.CalculateMedian(commandInputList, turnContext, cancellationToken);
+                        responseAttachment = ResponseCard.GetCardWithDecimalResult(median, command);
                         break;
                     case "range":
                         var range = this.statistics.CalculateRange(commandInputList, turnContext, cancellationToken);
+                        responseAttachment = ResponseCard.GetCardWithDecimalResult(range, command);
                         break;
                     case "variance":
                         var variance = this.statistics.CalculateVariance(commandInputList, turnContext, cancellationToken);
+                        responseAttachment = ResponseCard.GetCardWithDecimalResult(variance, command);
                         break;
                     case "mode":
                         var modeList = this.statistics.CalculateMode(commandInputList, turnContext, cancellationToken);
+                        responseAttachment = ResponseCard.GetCardWithIntArrayResult(modeList, command);
                         break;
                     case "standard deviation":
                         var stdDev = this.statistics.CalculateStandardDeviation(commandInputList, turnContext, cancellationToken);
+                        responseAttachment = ResponseCard.GetCardWithDecimalResult(stdDev, command);
                         break;
                     case "geometric mean":
                         var geometricMean = this.statistics.CalculateGeometricMean(commandInputList, turnContext, cancellationToken);
+                        responseAttachment = ResponseCard.GetCardWithDecimalResult(geometricMean, command);
                         break;
                     case "quadratic roots":
                         var quadRoots = this.geometrics.CalculateQuadraticRoots(commandInputList, turnContext, cancellationToken);
+                        responseAttachment = ResponseCard.GetCardWithStringResult(quadRoots, command);
                         break;
                     case "discriminant":
                         var discriminant = this.geometrics.CalculateDiscriminant(commandInputList, turnContext, cancellationToken);
+                        responseAttachment = ResponseCard.GetCardWithIntResult(discriminant, command);
                         break;
                     case "midpoint":
                         var midpoint = this.geometrics.CalculateMidpoint(commandInputList, turnContext, cancellationToken);
+                        responseAttachment = ResponseCard.GetCardWithStringResult(midpoint, command);
                         break;
                     case "distance":
                         var distance = this.geometrics.CalculateDistance(commandInputList, turnContext, cancellationToken);
+                        responseAttachment = ResponseCard.GetCardWithDoubleResult(distance, command);
                         break;
                     case "pythagorean triple":
                         var pythagoreanTriple = this.geometrics.CalculatePythagoreanTriple(commandInputList, turnContext, cancellationToken);
+                        responseAttachment = ResponseCard.GetCardWithStringResult(pythagoreanTriple, command);
                         break;
                     default:
                         await turnContext.SendActivityAsync(MessageFactory.Text(Resources.CannotPickUpCommandText), cancellationToken).ConfigureAwait(false);
                         break;
                 }
+
+                await turnContext.SendActivityAsync(MessageFactory.Attachment(responseAttachment), cancellationToken).ConfigureAwait(false);
             }
         }
 
